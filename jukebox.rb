2b7a0da926cc948746ffe1d05bda9c410c0868a4
Mendel Kramer <menkra@gmail.com>
2013-06-10 12:28:49 -0400

    Initial Commit

---
+++ jukebox.rb
@@ -0,0 +1,53 @@
+$songs = [
+  "The Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "The Phoenix - Consolation Prizes"
+]
+# list, play, help, exit
+
+def prompt_for_action
+  puts "\n"
+  puts 'What would you like to do next? (You can type "list", "play", "help", "exit")'
+  action = gets.chomp
+  args = action.split(" ")
+  action = args.shift
+  send action, *args
+end
+
+def list
+  $songs.each_index do |index|
+    puts "#{index+1}: #{$songs[index]}"
+  end
+  prompt_for_action
+end
+
+def play song_index = 1
+  puts "playing #{song_index[0] << ": " << $songs[song_index[0].to_i - 1]}"
+  prompt_for_action
+end
+
+def help
+  puts 'RTFM'
+  prompt_for_action
+end
+
+def exit
+end
+
+greeting = <<'TADA'
+
+  _   _            ____  _____    _  _____ _   _                            _       _        ____            
+ | |_| |__   ___  |  _ \| ____|  / \|_   _| | | |_ __ __ _  __ _  ___      | |_   _| | _____| __ )  _____  __
+ | __| '_ \ / _ \ | | | |  _|   / _ \ | | | |_| | '__/ _` |/ _` |/ _ \  _  | | | | | |/ / _ \  _ \ / _ \ \/ /
+ | |_| | | |  __/ | |_| | |___ / ___ \| | |  _  | | | (_| | (_| |  __/ | |_| | |_| |   <  __/ |_) | (_) >  < 
+  \__|_| |_|\___| |____/|_____/_/   \_\_| |_| |_|_|  \__,_|\__, |\___|  \___/ \__,_|_|\_\___|____/ \___/_/\_\
+                                                           |___/                                             
+Welcome to the JukeBox Hero!
+TADA
+
+puts greeting
+prompt_for_action
